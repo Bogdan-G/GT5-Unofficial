18129fa677fcdc887a4fd9740275de6cd5972074
Bogdan <bogdangtt@gmail.com>
2016-04-05 15:37:19 +0300

    cleanup .gitignore

--- src/main/java/gregtech/api/objects/GT_ArrayList.java
+++ src/main/java/gregtech/api/objects/GT_ArrayList.java
@@ -5,8 +5,7 @@ import java.util.Arrays;
 import java.util.Collection;
 
 public class GT_ArrayList<E> extends ArrayList<E> {
-    private static final long serialVersionUID = 1L;
-    private int size_sS;
+    private static final long serialVersionUID = 1L;private int size_sS;
 
     private final boolean mAllowNulls;
 
